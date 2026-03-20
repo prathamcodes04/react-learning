8c7572614ae0f7303591af7c32c486951909579e
Pratham Gupta <prathamgupta0405@gmail.com>
2026-03-20 18:08:16 +0530

    outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,49 @@
 import { useState } from "react";
 
-const Home = () => { 
-   // let name = 'mario';
-   const [name, setName] = useState('mario');
-   const [age, setAge] = useState(25);
+const Home = () => {
+   const[blogs, setBlogs] = useState([
+      {title: 'My new website', body:'lorem impsum...', author: 'mario', id: 1},
+      {title: 'Welcome party', body:'lorem impsum...', author: 'yoshi', id: 2},
+      {title: 'Web dev top tips', body:'lorem impsum...', author: 'mario', id: 3}
+   ]);
 
-   const handleClick = () => {
-      setName('luigi');
-      setAge(30);
-   }
-
-   // const handleClick = (e) => {
-   //    console.log('hello', e.target);
-   // }
-
-   // const handleClickAgain = (name, e) => {
-   //    console.log('hello ' + name, e.target);
-   // }
-
-   return (
+   return(
       <div className="home">
-         <h2>Homepage</h2>
-         <p>{name} is {age} years old</p>
-         <button onClick={handleClick}>Click Me!</button>
-         {/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
+         {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+               <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            </div>
+         ))}
       </div>
    );
+   
+   
+   // // let name = 'mario';
+   // const [name, setName] = useState('mario');
+   // const [age, setAge] = useState(25);
+
+   // const handleClick = () => {
+   //    setName('luigi');
+   //    setAge(30);
+   // }
+
+   // // const handleClick = (e) => {
+   // //    console.log('hello', e.target);
+   // // }
+
+   // // const handleClickAgain = (name, e) => {
+   // //    console.log('hello ' + name, e.target);
+   // // }
+
+   // return (
+   //    <div className="home">
+   //       <h2>Homepage</h2>
+   //       <p>{name} is {age} years old</p>
+   //       <button onClick={handleClick}>Click Me!</button>
+   //       {/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
+   //    </div>
+   // );
 };
 
 export default Home;
