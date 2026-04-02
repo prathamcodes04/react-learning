ce31b994b2d9e6748c6e070e67095ea9fa5cdcbe
Pratham Gupta <prathamgupta0405@gmail.com>
2026-04-02 23:25:40 +0530

    add new blog entry and implement navigation after submission

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {useNavigate} from "react-router-dom";
 
 const Create = () => {
     //storing user input
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,6 +23,7 @@ const Create = () => {
             }).then(() => {
                 console.log('new blog added');
                 setIsPending(false); //after completion
+                navigate('/');
             })
         }, 1000);
         // console.log(blog);
